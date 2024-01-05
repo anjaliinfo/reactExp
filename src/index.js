51e71669d70795c98b84148dcc8a9659a697a559
anjaliinfo <98171488+anjaliinfo@users.noreply.github.com>
2024-01-05 13:05:30 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import COMP_LIFE from './complife.jsx';
+import Form from './form.jsx';
+import EventExp from './events.jsx';
+
+ReactDOM.render(
+    <EventExp />,
+    document.getElementById('root')
+); 
+
+// ReactDOM.render(
+//     <Form />,
+//     document.getElementById('root')
+// ); 
+// ReactDOM.render(
+//     <COMP_LIFE />,
+//     document.getElementById('root')
+// ); 
